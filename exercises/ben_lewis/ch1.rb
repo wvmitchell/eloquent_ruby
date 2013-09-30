aab5e0faa59a04c10a0954008845b85f9f75e6ae
Nathaniel Watts <reg@nathanielwatts.com>
2013-09-30 15:29:22 -0600

    refactor tests to be refute instead of assert - as well as add some descriptions for testing a variable name

--- exercises/ben_lewis/ch1.rb
+++ exercises/ben_lewis/ch1.rb
@@ -6,21 +6,24 @@ class Chapter1Test < Minitest::Test
 
   def test_uses_snake_case_for_variables
     # write a variable name of at least two words in snake case
-    # replace all variable references below with the new variable name
+    # because you can't test the declaration of a variable name,
+    # write it as a string. Ex: "dog" or "variable" - but with two
+    # words.
 
     skip
-    # (variable) = test_var
-    assert variable[0] == variable[0].upcase
+    variable_name = # variable name example, as a string
+    refute variable[0] == variable[0].upcase
     assert variable.include?("_")
   end
 
   def test_uses_camel_case_for_classes
     # write a new class name of at least two words in camel case
-    # replace all class references at the bottom of this document with the new class name
+    # replace all class references at the bottom of this document 
+    # with the new class name
 
     skip
     class_name = (new class).return_name
-    assert_equal class_name.to_s[0], class_name.to_s[0].downcase
+    assert_refute class_name.to_s[0], class_name.to_s[0].downcase
     refute variable.include?("_", " ")
   end
 
@@ -28,7 +31,7 @@ end
 
 class (new class)
   def self.return_name
-    self
+    self.to_s
   end
 end
 
