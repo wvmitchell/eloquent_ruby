9fb97ac76811d9a6c9f1aba5f830f028302e9c6f
Simon Taranto <simon.taranto@gmail.com>
2013-10-06 16:12:17 -0600

    Add test file for ch6

---
+++ exercises/ch6.rb
@@ -0,0 +1,12 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class Chapter6Test < Minitest::Test
+
+  def test_it_works
+    answer = false
+    assert answer
+  end
+
+end
